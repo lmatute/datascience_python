78603bb4bb269bc993f1dc48dcf79c7c46c32f57
Luis Matute <luismatute@ymail.com>
2019-04-06 18:57:43 -0400

    initial commit

---
+++ startup.py
@@ -0,0 +1,5 @@
+import sys
+# store datascience modules in this folder
+sys.path.append('/private/var/mobile/Library/Mobile Documents/iCloud~com~omz-software~Pythonista3/Documents/datascience')
+# from vectorized import *
+# from statslm import *
